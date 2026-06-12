669080e9e2e422e009b37b9fafc010876bb8143a
AarshKamboj <kambojaarsh@gmail.com>
2026-06-12 14:40:26 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://biz-saathi-kappa.vercel.app",
-    ],
+    origin: true,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 
